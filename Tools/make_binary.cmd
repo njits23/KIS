4bb0216de79d7d64938197fda72d116a6163cf7a
njits23 <stijnreclame@xs4all.nl>
2026-04-26 21:20:50 +0200

    Update csproj and make_binary to work with my stuff

--- Tools/make_binary.cmd
+++ Tools/make_binary.cmd
@@ -1,3 +1,3 @@
 @echo off
 REM To get the latest MSBuild command line compiler go to: https://visualstudio.microsoft.com/thank-you-downloading-visual-studio/?sku=BuildTools&rel=16
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\amd64\MSBuild.exe" ..\Source\KIS.csproj /t:Clean,Build /p:Configuration=Release
+"C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\amd64\MSBuild.exe" ..\Source\KIS.csproj /t:Clean,Build /p:Configuration=Release
